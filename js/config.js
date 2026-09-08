bc8786c8a15ef38bebb760260d0a54fafaf0e810
xiaoouwl <82596548+xiaoouwl@users.noreply.github.com>
2026-09-08 22:18:13 +0800

    Update SITE_CONFIG for new site name and URL

--- js/config.js
+++ js/config.js
@@ -13,8 +13,8 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    name: '影视搜索',
+    url: '#',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
