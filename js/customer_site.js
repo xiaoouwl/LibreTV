b6d346616b29eef5c98ee05b4714761d9ed055c8
xiaoouwl <82596548+xiaoouwl@users.noreply.github.com>
2026-09-08 22:15:27 +0800

    Fix syntax error in CUSTOMER_SITES object

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,7 +2,7 @@ const CUSTOMER_SITES = {
     ruyi: {
         api: 'https://cj.rycjapi.com/api.php/provide/vod',
         name: '如意资源',
-    }
+    },
     dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
